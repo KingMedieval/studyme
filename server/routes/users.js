f4e0a59ea49feb3fe09d4b4e7051a3b22f48b217
KingMedieval <tshimada@purdue.edu>
2022-11-03 17:51:14 -0700

    sample math question added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const sr = require('seedrandom');
+const question = require('../public/templates/mathctemp.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users?');
+  let i = 0;
+  let variables = [];
+  for (const key in question.variables) {
+    console.log(`${key}: ${question.variables[key]}`);
+    if (question.variables[key].type === 'int') {
+      variables[i] = Math.floor(Math.random() * (question.variables[key].max - question.variables[key].min + 1) + question.variables[key].min);
+    }
+    else {
+      variables[i] = Math.random() * (question.variables[key].max - question.variables[key].min + 1) + question.variables[key].min;
+      variables[i] = variables[i].toFixed(question.variables[key].prec);
+    }
+    i++;
+  }
+
+  res.send(JSON.stringify(question).replace(/\${c1}/g,variables[0]).replace(/\${c2}/g,variables[1]).replace(/\${c3}/g,variables[2]));
 });
 
 module.exports = router;
